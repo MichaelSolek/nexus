1790251772991d1e423cb6867881f46dcf3b19bd
MichaelSolek <michael.solek@mavs.uta.edu>
2022-06-24 13:39:29 -0500

    Divide detector into three field regions

--- source/geometries/CRAB.cc
+++ source/geometries/CRAB.cc
@@ -104,20 +104,22 @@ namespace nexus{
 
 
         //Creating the Steel Cylinder that we use
-        G4Tubs* chamber_solid =new G4Tubs("CHAMBER", chamber_diam/2, (chamber_diam/2. + chamber_thickn),(chamber_length/2. + chamber_thickn), 0.,twopi);
-        G4LogicalVolume* chamber_logic =new G4LogicalVolume(chamber_solid,materials::Steel(), "CHAMBER"); //
-        // Placing the gas in the chamber
+        G4Tubs* chamber_solid = new G4Tubs("CHAMBER", chamber_diam/2, (chamber_diam/2. + chamber_thickn),(chamber_length/2. + chamber_thickn), 0.,twopi);
+        G4LogicalVolume* chamber_logic = new G4LogicalVolume(chamber_solid,materials::Steel(), "CHAMBER"); //
 
-        G4Tubs* gas_solid =new G4Tubs("GAS", 0., chamber_diam/2., chamber_length/2., 0., twopi);
-        G4Tubs* Active_solid =new G4Tubs("ACTIVE", 0., Active_diam/2., Active_length/2., 0., twopi);
+	// Placing the gas in the chamber and define different field regions
+        G4Tubs* gas_solid = new G4Tubs("GAS", 0., chamber_diam/2., chamber_length/2., 0., twopi);
+        G4Tubs* fieldCage_solid = new G4Tubs("FIELD_CAGE", 0., Active_diam/2., (807.157 * mm)/2., 0., twopi); //length is model distance from edge of cathode to edge of nearest EL ring
+	G4Tubs* EL_solid = new G4Tubs("EL_GAP", 0., 382. * mm, (6.628 * mm)/2., 0, twopi);
+	G4Tubs* beyondEL_solid = new G4Tubs("BEYOND_EL", 0., Active_diam/2., (7.022 * 25.4 * mm)/2., 0., twopi);
 
         // Radioactive Source Encloser
         //Source
         //G4Tubs* SourceHolChamber_solid =new G4Tubs("SourceHolChamber", SourceEn_holedia/2, (SourceEn_diam/2. + SourceEn_thickn),(SourceEn_length/2. + SourceEn_thickn),0,twopi);
-        //G4Tubs* SourceHolChamberBlock_solid =new G4Tubs("SourceHolChBlock",0,(SourceEn_holedia/2),( SourceEn_thickn/2), 0.,twopi);
+	//G4Tubs* SourceHolChamberBlock_solid =new G4Tubs("SourceHolChBlock",0,(SourceEn_holedia/2),( SourceEn_thickn/2), 0.,twopi);
 
 
-      //G4VSolid *SourceHolderGas_solid= new G4SubtractionSolid("SourceHolderGas",Source_Chm_solid,SourceHolder_solid);
+        //G4VSolid *SourceHolderGas_solid= new G4SubtractionSolid("SourceHolderGas",Source_Chm_solid,SourceHolder_solid);
 
 
         //G4Material* gxe = materials::GXe(gas_pressure_);
@@ -127,7 +129,9 @@ namespace nexus{
 
         gxe->SetMaterialPropertiesTable(opticalprops::GXe(gas_pressure_, 68));
         G4LogicalVolume* gas_logic = new G4LogicalVolume(gas_solid, gxe, "GAS");
-        G4LogicalVolume* Active_logic = new G4LogicalVolume(Active_solid, gxe, "ACTIVE");
+        G4LogicalVolume* fieldCage_logic = new G4LogicalVolume(fieldCage_solid, gxe, "FIELD_CAGE");
+	G4LogicalVolume* EL_logic = new G4LogicalVolume(EL_solid, gxe, "EL_GAP");
+	G4LogicalVolume* beyondEL_logic = new G4LogicalVolume(beyondEL_solid, gxe, "BEYOND_EL");
 
 
         //G4LogicalVolume* SourceHolderGas_logic = new G4LogicalVolume(SourceHolderGas_solid, gxe, "SourceHolderGAS_logic");
@@ -143,10 +147,12 @@ namespace nexus{
         //rm->rotateX(-45.*deg);
 
         // Place the Volumes
-        new G4PVPlacement(0,G4ThreeVector(),lab_logic_volume,lab_logic_volume->GetName(),0,false,0, true);
-        new G4PVPlacement(0,G4ThreeVector(0.,0.,0.) ,chamber_logic, chamber_solid->GetName(), lab_logic_volume, false, 0,true);
-        new G4PVPlacement(0, G4ThreeVector(0.,0.,0.), gas_logic, gas_solid->GetName(),chamber_logic, false, 0, true);
-        new G4PVPlacement(0, G4ThreeVector(0.,0.,0.), Active_logic, Active_solid->GetName(),gas_logic, false, 0, true);
+        new G4PVPlacement(0, G4ThreeVector(),lab_logic_volume,lab_logic_volume->GetName(),0,false,0, true);
+        new G4PVPlacement(0, G4ThreeVector(0., 0., 0.            ) ,chamber_logic, chamber_solid->GetName(), lab_logic_volume, false, 0,true);
+        new G4PVPlacement(0, G4ThreeVector(0., 0., 0.            ), gas_logic, gas_solid->GetName(),chamber_logic, false, 0, true);
+        new G4PVPlacement(0, G4ThreeVector(0., 0., -15.8094 * mm ), fieldCage_logic, fieldCage_solid->GetName(),gas_logic, false, 0, true); // offset calculated based off model
+	new G4PVPlacement(0, G4ThreeVector(0., 0., -436.7015 * mm), EL_logic, EL_solid->GetName(), gas_logic, false, 0, true);
+	new G4PVPlacement(0, G4ThreeVector(0., 0., -545.3015 * mm), beyondEL_logic, beyondEL_solid->GetName(), gas_logic, false, 0, true);
         //new G4PVPlacement(rm, G4ThreeVector(-SourceEn_offset,-SourceEn_offset,-SourceEn_offset), SourceHolChamber_logic, SourceHolChamber_solid->GetName(),gas_logic, false, 0, true);
         //new G4PVPlacement(rm, G4ThreeVector(-SourceEn_offset-SourceEn_length/2,-SourceEn_offset-SourceEn_length/2,-SourceEn_offset), SourceHolChamberBlock_logic, SourceHolChamberBlock_solid->GetName(),gas_logic, false, 0, true);
        // new G4PVPlacement(rm, G4ThreeVector(-SourceEn_offset,0,0), SourceHolChamber_logic, SourceHolChamber_solid->GetName(),gas_logic, false, 0, true);
@@ -154,10 +160,16 @@ namespace nexus{
 
 
         // Define this volume as an ionization sensitive detector
-        IonizationSD* sensdet = new IonizationSD("/CRAB/ACTIVE");
+        IonizationSD* sensdet_fc = new IonizationSD("/CRAB/FIELD_CAGE");
+	IonizationSD* sensdet_el = new IonizationSD("/CRAB/EL_GAP");
+	IonizationSD* sensdet_bel = new IonizationSD("/CRAB/BEYOND_EL");
         //IonizationSD* sensdet = new IonizationSD("/CRAB/GAS");
-        Active_logic->SetSensitiveDetector(sensdet);
-        G4SDManager::GetSDMpointer()->AddNewDetector(sensdet);
+        fieldCage_logic->SetSensitiveDetector(sensdet_fc);
+	EL_logic->SetSensitiveDetector(sensdet_el);
+	beyondEL_logic->SetSensitiveDetector(sensdet_bel);
+        G4SDManager::GetSDMpointer()->AddNewDetector(sensdet_fc);
+	G4SDManager::GetSDMpointer()->AddNewDetector(sensdet_el);
+	G4SDManager::GetSDMpointer()->AddNewDetector(sensdet_bel);
 
         AssignVisuals();
         //this->SetLogicalVolume(lab_logic_volume);
@@ -171,7 +183,7 @@ namespace nexus{
         G4LogicalVolumeStore* lvStore = G4LogicalVolumeStore::GetInstance();
         G4LogicalVolume* Chamber = lvStore->GetVolume("CHAMBER");
         G4LogicalVolume* Lab = lvStore->GetVolume("LAB");
-        G4LogicalVolume* Active = lvStore->GetVolume("ACTIVE");
+        G4LogicalVolume* Active = lvStore->GetVolume("FIELD_CAGE");
         G4LogicalVolume* Gas = lvStore->GetVolume("GAS");
 
         //G4LogicalVolume* SourceHolder = lvStore->GetVolume("SourceHolChamber_logic");
@@ -207,7 +219,7 @@ namespace nexus{
     G4ThreeVector CRAB::GenerateVertex(const G4String& region) const
     {
 
-        if(!(region=="LAB" || region=="GAS" || region=="ACTIVE" || region=="SourceHolChamber")){
+        if(!(region=="LAB" || region=="GAS" || region=="FIELD_CAGE" || region=="SourceHolChamber")){
 
             G4Exception("[CRAB]", "GenerateVertex()", FatalException,
                         "Unknown vertex generation region.");
